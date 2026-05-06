7c66bfacaf415d981991ccc9f961918df443c6fb
DanhVinhLe <danhvinhlee@gmail.com>
2026-05-06 23:37:37 +0000

    Fix emo

--- src/criterions/emo_loss.py
+++ src/criterions/emo_loss.py
@@ -60,8 +60,8 @@ class EMOLoss(nn.Module):
         teacher_qry_input = input_data['teacher_inputs']['qry']
         teacher_pos_input = input_data['teacher_inputs']['pos']
         
-        num_text_qry_tokens = ((teacher_qry_input['input_ids'] < 151652) | (teacher_qry_input['input_ids'] > 151656)).sum(dim=1)
-        num_text_pos_tokens = ((teacher_pos_input['input_ids'] < 151652) | (teacher_pos_input['input_ids'] > 151656)).sum(dim=1)
+        num_text_qry_tokens = (((teacher_qry_input['input_ids'] < 151652) | (teacher_qry_input['input_ids'] > 151656)) & (teacher_qry_input['input_ids'] != 151643)).sum(dim=1)
+        num_text_pos_tokens = (((teacher_pos_input['input_ids'] < 151652) | (teacher_pos_input['input_ids'] > 151656)) & (teacher_pos_input['input_ids'] != 151643)).sum(dim=1)
         batch_size = student_qry_input['input_ids'].size(0)
         with torch.no_grad():
             teacher_model.eval()
